5f182f167c7c0c86de027e99379c313e6efec973
Guilherme Eiti Akita Miua <guilhermemiua@gmail.com>
2019-06-27 02:12:19 -0300

    Add instructor register and login

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,20 @@ const express = require("express");
 const router = express.Router();
 
 const bodybuilderRegisterService = require("../services/bodybuilder/register/register");
+const instructorRegisterService = require("../services/instructor/register/register");
 
 router.post("/bodybuilder/register", (req, res) =>
   bodybuilderRegisterService.register(req, res)
 );
 router.post("/bodybuilder/login", (req, res) =>
   bodybuilderRegisterService.login(req, res)
+);
+
+router.post("/instructor/register", (req, res) =>
+  instructorRegisterService.register(req, res)
+);
+router.post("/instructor/login", (req, res) =>
+  instructorRegisterService.login(req, res)
 );
 
 module.exports = router;
