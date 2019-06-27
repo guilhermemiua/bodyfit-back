75e479b9058c81a668b7d400976668a49c494c6c
Guilherme Eiti Akita Miua <guilhermemiua@gmail.com>
2019-06-27 15:12:34 -0300

    Fixed project structure

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
-const bodybuilderRegisterService = require("../services/bodybuilder/register/register");
-const instructorRegisterService = require("../services/instructor/register/register");
+const registerService = require("../services/register/register");
 
 router.post("/bodybuilder/register", (req, res) =>
-  bodybuilderRegisterService.register(req, res)
+  registerService.bodybuilderRegister(req, res)
 );
 router.post("/bodybuilder/login", (req, res) =>
-  bodybuilderRegisterService.login(req, res)
+  registerService.bodybuilderLogin(req, res)
 );
 
 router.post("/instructor/register", (req, res) =>
-  instructorRegisterService.register(req, res)
+  registerService.instructorRegister(req, res)
 );
 router.post("/instructor/login", (req, res) =>
-  instructorRegisterService.login(req, res)
+  registerService.instructorLogin(req, res)
 );
 
 module.exports = router;
